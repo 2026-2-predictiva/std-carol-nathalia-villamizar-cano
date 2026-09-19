f1cd889c8cbb6315915afa1599492e49b161c64d
cvillamizarc <cvillamizarc@unal.edu.co>
2026-09-19 11:11:07 -0500

    Intento_1

--- PRE_01_hola_mundo/src/pregunta_01.py
+++ PRE_01_hola_mundo/src/pregunta_01.py
@@ -1,5 +1,5 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
+Escriba el codigo que ejecute la accion solicitada en cada pregunta..
 """
 
 # pylint: disable=import-outside-toplevel
@@ -7,11 +7,11 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 def pregunta_01():
     """
-    Retorne el string "Hola mundo cruel!".
+    Retorne el string "Hola mundo cruel!"
 
     """
 
-    return "Hola mundo cruel!"
+    return 'Hola mundo cruel!'
 
 
 if __name__ == "__main__":
