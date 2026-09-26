c717ddcc93d4c6193868cabd90c0bf2bff5f09f0
cvillamizarc <cvillamizarc@unal.edu.co>
2026-09-26 11:47:14 -0500

    Pre_7

--- PRE_01_hola_mundo/src/pregunta_01.py
+++ PRE_01_hola_mundo/src/pregunta_01.py
@@ -8,7 +8,6 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 def pregunta_01():
     """
     Retorne el string "Hola mundo cruel!".
-
     """
 
     return "Hola mundo cruel!"
--- PRE_01_hola_mundo/src/pregunta_02.py
+++ PRE_01_hola_mundo/src/pregunta_02.py
@@ -8,8 +8,6 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 def pregunta_02():
     """
     Retorne el string "Hello cruel world!".
-
-
     """
 
     return "Hello cruel world!"
